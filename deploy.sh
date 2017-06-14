282ec44d5c56e671fca7371dabf7d668dfca0cd2
Firedrake969 <firedrake969@users.noreply.github.com>
2017-06-14 10:30:22 -0600

    add pip install

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 rm -rf project_name && \
 	yes "" | cookiecutter . && \
 	cd project_name && \
+	pip install -r requirements/production.txt && \
 	python manage.py migrate && \
 	gunicorn config/wsgi.py
